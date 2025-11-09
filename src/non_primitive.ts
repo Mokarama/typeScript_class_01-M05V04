83b56b4743fd63c410e39a3c097da8080e4d9d48
Mokarama <mokaramaaktershanta@gmail.com>
2025-11-09 19:24:30 +0600

    Added primitive & non-primitive data type

---
+++ src/non_primitive.ts
@@ -0,0 +1,19 @@
+//array, object
+
+let bazarList: string[]  =['eggs', 'milk', 'sugar'];
+
+bazarList.push(true)// string e hote hbe
+
+let mixedArr: ( string | number)[] =["eggs", 12, "milk", 1, "sugar", 4];
+
+mixedArr.push('apple')
+
+
+
+let coordinates: [number, number] =[20, 30];
+
+let couple: [string, string] = ['Husband', 'Wife'];
+
+let shantaNameAndRoll:[string, number] = ["Shanta", 79];
+
+let destination:[string, string,number] =['Dhaka', 'Ctg',12 ]
---
+++ src/primitive.ts
@@ -0,0 +1,21 @@
+//   JS - TS
+//string , number , boolean, undefined, null
+
+//TS: never, unknown, void
+
+
+let userName:string ="Shanta123";
+userName="123";
+
+let userId:number=123;
+
+userName.includes;
+userId.toFixed;
+
+
+
+let isAdmin: boolean=false;
+isAdmin =true;
+
+let x: undefined=undefined;
+
